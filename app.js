31033e80003873013410f3143c15a2f447fb1173
Erin Cameron <erincameron11@hotmail.com>
2019-12-20 11:11:25 -0500

    edit all files

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 // from video: https://www.youtube.com/watch?v=6iZiqQZBQJY
 
-let express = require('express');
-let app = express(); // Create the app
-let fs = require('fs'); // File System for loading the list of contacts
+// AUTHENTICATION 
+//require('dotenv').config({client: 'pg'});
+const express = require('express');
+const app = express(); // Create the app
+const fs = require('fs'); // File System for loading the list of contacts
+const session = require('express-session');
+const server = express();
 
-// Our "database" is "additional.json", check first to see if it exists
+// AUTHENTICATION
+// const KnexSessionStore = require('connect-session-knex')(session);
+// const knex = require('knex');
+// const knexConfig = require('../fs1020-project/node_modules/knex/knex.js');
+// const db = knex(knexConfig);
+
+
+
+
+// Check first to see if our database exists
 let contacts;
 let exists = fs.existsSync('contacts.json');
 if (exists) {
@@ -19,31 +32,50 @@ if (exists) {
   contacts = {};
 }
 
-// Set up the server
-// process.env.PORT is related to deploying on heroku
-let server = app.listen(process.env.PORT || 3000, listen);
 
-// This call back just tells us that the server has started
-function listen() {
-  let host = server.address().address;
-  let port = server.address().port;
-}
 
-// A route for adding a new word with a score
-app.get('/add/:name/:email/:phone', addContact);
+// LISTEN ON PORT
+// since ports are not always constantly the same number, this is the proper way to set up a port
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
 
+
+
+
+// THIS IS THE AUTHENTICATION PORTION WE APPARENTLY NEED
+// server.use(session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new KnexSessionStore({ knex: db }),
+//     cookie: {
+//       maxAge: 1200000, // 20 minutes
+//       httpOnly: true,
+//       secure: process.env.NODE_ENV === 'production',
+//     },
+//   }));
+
+
+
+/** Fields required: Name, Email, Phone Number - when
+ * submitted should be sent to JSON file, and when rejected
+ * proper HTTP status code should show
+ */
+
+// ROUTE FOR ADDING A NEW CONTACT WITH NAME, EMAIL AND PHONE
+app.post('/user/:name/:email/:phone', addContact);
 // Handle that route
 function addContact(req, res) {
-  // Word and score
+  // Name, Email and Phone
   let name = req.params.name;
   let email = req.params.email;
-  // Make sure it's not a string by accident
-  let phone = req.params.phone;
-
+  let phone = Number(req.params.phone);
+  let id = ([contacts.length++]); // NEED TO FIX THIS
   // Put it in the object
-  contacts[name] = email + ' ' + phone;
-
-  // Let the request know it's all set
+  contacts[id] = {name, email, phone};
+  // Let the request know it was sent properly
   let reply = {
     status: 'success',
     name: name,
@@ -51,24 +83,60 @@ function addContact(req, res) {
     phone: phone
   }
   console.log('adding: ' + JSON.stringify(reply));
-
-  // Write a file each time we get a new word
-  // This is kind of silly but it works
+  // Write a file each time we get a new contact
   let json = JSON.stringify(contacts, null, 2);
   fs.writeFile('contacts.json', json, 'utf8', finished);
   function finished(err) {
     console.log('Finished writing contacts.json');
-    // Don't send anything back until everything is done
+    // Don't send anything back until everything else is done
     res.send(reply);
   }
 }
 
-// Route for sending all the concordance data
-app.get('/all', showAll);
+  // ROUTE TO CREATE AN ENTRY WHEN THE USER SUBMITS THEIR FORM
+  app.get('/submit', function(req,res) {
+    res.send('Thank you for submitting the contact us form! Your data has been sent to our JSON file and stored.');
+  });
 
-// Callback
-function showAll(req, res) {
-  // Send the entire dataset
-  // express automatically renders objects as JSON
-  res.send(contacts);
-}
+
+  // ROUTE TO CREATE OR REGISTER A USER
+  app.post('/id/:user/:password', function(req, res) {
+    let user = req.params.user;
+    let password = req.params.password;
+    exports.getUserById = function(id) {
+        for (var i = 0; i < users.length; i++) {
+          if (users[i].id == id) return users[i];
+        }
+      };
+    // let id = ([contacts.length++]);
+    // Put it in the object
+    contacts[id] = {user, password};
+    let reply = {
+        status: 'success',
+        user: user,
+      }
+      console.log('adding: ' + (user + ' and super secret password'));
+      let json = JSON.stringify(contacts, null, 2);
+      fs.writeFile('contacts.json', json, 'utf8', finished);
+      function finished(err) {
+        console.log('Finished writing contacts.json with new user');
+        // Don't send anything back until everything else is done
+        res.send(reply);
+      }
+  });
+
+  // ROUTE TO LOG A REGISTERED USER IN TO CREATE A SESSION
+  app.post('/login/:user/:password', function (req, res) {
+    let user = contacts[name];
+    if(!contact) {
+        res.send('That username is not correct');
+    } else {
+        res.send('You are loggen in!');
+    }
+  });
+
+
+  // ROUTE TO GET A LISTING OF ALL SUBMISSIONS
+app.get('/all', function (req, res) {
+    res.send(contacts);
+});
